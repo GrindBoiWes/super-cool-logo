5f6f83f7ddbdfb01df094a32ccbcfd2d8303b068
Wes Harrison <wesharrison89@gmail.com>
2023-03-10 22:17:24 -0800

    updated inquirer prompt and added code to shapes.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const SVG = require('./examples');
 const {Square, Circle, Triangle} = require('./lib/shapes');
 
-const questions = [
+function init () {
+inquirer
+ .prompt ([
    
     {
         type: 'input',
@@ -24,7 +26,8 @@ const questions = [
             'Square',
             'Circle',
             'Triangle',
-        ]
+        ],
+        name: 'shape',
     },
 
     {
@@ -32,4 +35,29 @@ const questions = [
         message: 'What color would you the shape to be? (Enter keyword or hex value',
         name: 'shapeColor'
     }
-]
+  ])
+  
+  .then(({text, textColor, shape, shapeColor}) => {
+    let svg;
+    switch (shape) {
+        case 'Square':
+            svg = new Square(text, textColor, shapeColor)
+            break;
+        
+        case 'Circle':
+            svg = new Circle(text, textColor, shapeColor)
+            break;
+
+        case 'Triangle':
+            svg = new Triangle(text, textColor, shapeColor)
+            break;
+    }
+    const svgString = SVG(svg);
+    fs.writeFile('logo.svg', svgString, function (err) {
+        if (err) throw err;
+        console.log('Your new logo has been created!')
+    })
+  });
+}
+
+init();
